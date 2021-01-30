d58cace881937e5f2e329c37ec1d2748a0a5835f
Henrique Silva <jhsv09@gmail.com>
2021-01-30 22:29:43 +0000

    [DNM] sm8250-common: Introduce 'SafailNet'

--- common.mk
+++ common.mk
@@ -219,6 +219,7 @@ PRODUCT_PACKAGES += \
     init.qti.ims.sh \
     init.qti.qcv.sh \
     init.recovery.qcom.rc \
+    init.safailnet.rc \
     init.target.rc \
     ueventd.qcom.rc \
     vendor.oem_ftm.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -249,3 +249,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.oneplus.display.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
+
+#Saifailnet
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.safailnet.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.safailnet.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
